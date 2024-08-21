bd5128c500e6bccd0574abb6be7446963c610bdd
lucasbonaffini <lucasbonaffini@gmail.com>
2024-08-21 14:03:37 -0300

    Add CI configuration with GitHub Actions

--- src/__tests__/services/ClassService.test.js
+++ src/__tests__/services/ClassService.test.js
@@ -52,64 +52,64 @@ describe('ClassService', () => {
   test('should return a class by id', async () => {
     const mockClass = { id: '1', name: 'Wizard', description: 'A powerful wizard', attributes: {} };
     pool.query.mockResolvedValue([[mockClass]]);
-
+  
     const classInstance = await ClassService.getClassById('1');
-
+  
     expect(pool.query).toHaveBeenCalledWith('SELECT * FROM classes WHERE id = ?', ['1']);
     expect(classInstance).toEqual(mockClass);
   });
-
+  
   test('should return null if class not found by id', async () => {
-    pool.query.mockResolvedValue([[]]); // Simula que no se encontró la clase
-
+    pool.query.mockResolvedValue([[]]);
+  
     const classInstance = await ClassService.getClassById('999');
-
+  
     expect(pool.query).toHaveBeenCalledWith('SELECT * FROM classes WHERE id = ?', ['999']);
     expect(classInstance).toBeNull();
   });
-
+  
   test('should update a class', async () => {
     const mockClass = { id: '1', name: 'Wizard', description: 'A powerful wizard', attributes: {} };
     const updatedClassData = { name: 'Sorcerer', description: 'Master of magic', attributes: {} };
-    pool.query.mockResolvedValue([{ affectedRows: 1 }]);
+    pool.query.mockResolvedValueOnce([{ affectedRows: 1 }]);
     pool.query.mockResolvedValueOnce([[mockClass]]);
-
+  
     const updatedClass = await ClassService.updateClass('1', updatedClassData);
-
+  
     expect(pool.query).toHaveBeenCalledWith(
       'UPDATE classes SET name = ?, description = ?, attributes = ? WHERE id = ?',
       [updatedClassData.name, updatedClassData.description, JSON.stringify(updatedClassData.attributes), '1']
     );
     expect(updatedClass).toEqual(mockClass);
   });
-
+  
   test('should return null if class not found for update', async () => {
     const updatedClassData = { name: 'Sorcerer', description: 'Master of magic', attributes: {} };
-    pool.query.mockResolvedValue([{ affectedRows: 0 }]); // Simula que no se encontró la clase para actualizar
-
+    pool.query.mockResolvedValue([{ affectedRows: 0 }]); 
+  
     const updatedClass = await ClassService.updateClass('999', updatedClassData);
-
+  
     expect(pool.query).toHaveBeenCalledWith(
       'UPDATE classes SET name = ?, description = ?, attributes = ? WHERE id = ?',
       [updatedClassData.name, updatedClassData.description, JSON.stringify(updatedClassData.attributes), '999']
     );
     expect(updatedClass).toBeNull();
   });
-
+  
   test('should delete a class by id', async () => {
     pool.query.mockResolvedValue([{ affectedRows: 1 }]);
-
+  
     const result = await ClassService.deleteClass('1');
-
+  
     expect(pool.query).toHaveBeenCalledWith('DELETE FROM classes WHERE id = ?', ['1']);
     expect(result).toEqual(1);
   });
-
+  
   test('should return null if class not found for deletion', async () => {
-    pool.query.mockResolvedValue([{ affectedRows: 0 }]); // Simula que no se encontró la clase para eliminar
-
+    pool.query.mockResolvedValue([{ affectedRows: 0 }]); 
+  
     const result = await ClassService.deleteClass('999');
-
+  
     expect(pool.query).toHaveBeenCalledWith('DELETE FROM classes WHERE id = ?', ['999']);
     expect(result).toBeNull();
   });
